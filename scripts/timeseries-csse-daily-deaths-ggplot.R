e66ace0795d8abffce7e8e25134d84c5bf7fc912
Brent Thorne <brent.thorne18@gmail.com>
2020-03-25 20:34:45 -0400

    daily update

---
+++ scripts/timeseries-csse-daily-deaths-ggplot.R
@@ -0,0 +1,67 @@
+library(tidyverse)
+library(gganimate)
+library(ggrepel)
+library(ggdark)
+
+ggplot() +
+  
+  #add trend line
+  geom_line(data = covid19_day1,
+              aes(x = Day,
+                  y = Deaths_Daily,
+                  colour = Country), 
+            stat = "smooth",
+            alpha = 0.5,
+            method = "gam") +
+  
+  #add points
+  geom_point(data = covid19_day1,
+             aes(x = Day,
+                 y = Deaths_Daily,
+                 alpha = Day,
+                 colour = Country,
+                 size = Deaths_Daily)) +
+  
+  # #add points
+  # geom_line(data = covid19_day1,
+  #            aes(x = Day,
+  #                y = Deaths_Daily,
+  #                colour = Country)) +
+  
+  geom_text(data = covid19_day1 %>%
+              filter(Date == max(Date),
+                     Country != "Canada"),nudge_x = 1.5, nudge_y = 16,
+            aes(x = Day,
+                y = Deaths_Daily,
+                label = Country,
+                colour = Country),
+            size = 5, alpha = 0.95,  family = "bold") +
+  
+  geom_text(data = covid19_day1 %>%
+              filter(Date == max(Date),
+                     Country == "Canada"),nudge_x = 2, nudge_y = -15,
+            aes(x = Day,
+                y = Deaths_Daily,
+                label = Country,
+                colour = Country),
+            size = 5, alpha = 0.95,  family = "bold") +
+  
+  #customize the theme
+  dark_mode(theme_fivethirtyeight()) +
+  
+  labs(title = paste("COVID-19 New Daily Deaths"),
+       x = "Days Since First Death",
+       y = "Deaths per Day",
+       subtitle = "_Day 1 = First COVID-19 Death in Country_ <br> _Data Source: Johns Hopkins CSSE_") +
+  
+  theme(legend.position = "none",
+        plot.title = element_markdown(size = 30),
+        plot.subtitle = element_markdown(size = 16),
+        axis.title = element_markdown(size = 14),
+        axis.text = element_text(size = 12,face = "bold")) +
+  
+  # scale_colour_colorblind() +
+  
+  NULL
+
+ggsave("figures/covid19-daily-deaths.png", width = 11.8, height = 8.68)
--- scripts/timeseries_csse_covid19_dailydeath_graphs.R
+++ scripts/timeseries_csse_covid19_dailydeath_graphs.R
@@ -1,24 +1,18 @@
 library(tidyverse)
-library(ggthemes)
-library(ggtext)
 library(lubridate)
 
 covid19_tidy <- read_csv("csse_covid19_timeseries_combined_tidy.csv")
 
-countries_of_interest <- c("China", "US","Spain","Canada","France","Germany")
+countries_of_interest <- c("Italy","US","Spain","Canada","France","Germany")
 # countries_of_interest <- c("France")
+
+covid19_day1 <- c()
 
 i = 1
 
 for (i in 1:length(countries_of_interest)) {
   
 country <- countries_of_interest[i]
-
-covid_italy <- covid19_tidy %>% 
-  filter(Country == "Italy",
-         Deaths_Total > 0)
-
-covid_italy$Day <- 1:length(covid_italy$Date)
 
 covid_other <- covid19_tidy %>% 
   filter(Country == country,
@@ -27,36 +21,7 @@ covid_other <- covid19_tidy %>%
   summarise(Deaths_Daily = sum(Deaths_Daily))
 
 covid_other$Day <- 1:length(covid_other$Date)
-
-deaths_total_italy <- covid_italy$Deaths_Total[length(covid_italy$Deaths_Total)]
-
-ggplot() +
-  
-  #add trend line
-  stat_smooth(data = covid_italy, aes(x = Day, y = Deaths_Daily), colour = "#000000", alpha = 0.1, method = "gam", fill = NA) +
-  stat_smooth(data = covid_other, aes(x = Day, y = Deaths_Daily), colour = "#cc0000", alpha = 0.1, method = "gam", fill = NA) +
-  
-  #add points
-  geom_point(data = covid_italy, aes(x = Day, y = Deaths_Daily, size = Deaths_Daily), colour = "#00000050") +
-  geom_point(data = covid_other, aes(x = Day, y = Deaths_Daily, size = Deaths_Daily), colour = "#cc000050") +
-  
-  #add text
-  geom_text(data = covid_italy[c(1,length(covid_italy$Date)),], aes(x = Day, y = Deaths_Daily + 25, label = Date), colour = "#000000", size = 5) +
-  geom_text(data = covid_other[c(1,length(covid_other$Date)),], aes(x = Day, y = Deaths_Daily - 20, label = Date), colour = "#cc0000", size = 5) +
-  
-  #customize the theme
-  theme_fivethirtyeight() +
-  labs(title = paste("COVID-19 Daily Deaths for **<span style='color:#000000'>Italy**</span> & **<span style='color:#cc0000'>", country, "</span>**", sep = ""),
-       x = "Days Since First Death",
-       y = "Deaths per Day",
-       subtitle = "_Day 1 = First COVID-19 Death in Country_") +
-  theme(legend.position = "none",
-        plot.title = element_markdown(size = 30),
-        plot.subtitle = element_markdown(size = 16),
-        axis.title = element_markdown(size = 14)) +
-  NULL
-
-#save plot
-ggsave(paste0("figures/covid-19_daily_deaths_italy_",country,".png"), width = 14, height = 7)
+covid_other$Country <- country
+covid19_day1 <- rbind(covid19_day1,covid_other)
 
 }
