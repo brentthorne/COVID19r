bdd0fec79f41642141d298f5d061c467fa3576f8
Brent Thorne <brent.thorne18@gmail.com>
2020-03-25 21:29:50 -0400

    add name to plot and remove video

--- scripts/timeseries_csse_covid19_dailydeath_graphs.R
+++ scripts/timeseries_csse_covid19_dailydeath_graphs.R
@@ -3,7 +3,7 @@ library(lubridate)
 
 covid19_tidy <- read_csv("csse_covid19_timeseries_combined_tidy.csv")
 
-countries_of_interest <- c("Italy","US","Spain","Canada","France","Germany")
+countries_of_interest <- c("Italy","US","Spain","Iran", "Canada","France","Germany")
 # countries_of_interest <- c("France")
 
 covid19_day1 <- c()
