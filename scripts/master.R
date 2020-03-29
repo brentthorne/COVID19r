9f93b055417e80359d7805e72a89bdcda600414b
Brent Thorne <brent.thorne18@gmail.com>
2020-03-28 21:29:22 -0400

    one script to bind them, one script to rule them all

---
+++ scripts/master.R
@@ -0,0 +1,5 @@
+source("scripts/timeseries_csse_covid19_tidy_script.R")
+source("scripts/timeseries_csse_covid19_dailyconfirmed_script.R")
+source("scripts/timeseries_csse_covid19_dailydeath_script.R")
+source("scripts/timeseries-csse-daily-confirmed-ggplot.R")
+source("scripts/timeseries-csse-daily-deaths-ggplot.R")
